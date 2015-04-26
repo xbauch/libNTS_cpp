204e12a5e84d3b17522e97d423adff8c7d8bda1e
Jan Tusil <410062@mail.muni.cz>
2015-04-26 22:29:26 +0200

    header

---
+++ inliner/inliner.hpp
@@ -0,0 +1,21 @@
+#ifndef NTS_INLINER_HPP_
+#define NTS_INLINER_HPP_
+#pragma once
+
+#include <libNTS/logic.hpp>
+#include <libNTS/nts.hpp>
+
+/**
+ * @brief Substitute variables inside formula with their shadow variables.
+ * @pre Each variable in formula must have 'user_data' set to nullptr
+ * or pointing to another variable.
+ * @post Occurences of variables, which had 'user data' pointing to some other variable,
+ * are substituted with the other variable. Nothing else is modified.
+ */
+void substitute_variables ( nts::Formula & f );
+void substitute_variables ( nts::Term & t );
+
+void inline_calls ( nts::BasicNts & bn );
+
+
+#endif // NTS_INLINER_HPP_
