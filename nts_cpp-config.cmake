3232fbabca86894d81280060d5f4a9c2f14bcf36
Jan Tusil <410062@mail.muni.cz>
2015-04-30 21:02:23 +0200

    CMake's 'find_package' works as desired

--- nts_cpp-config.cmake
+++
@@ -1,3 +0,0 @@
-add_library(NTS_cpp SHARED IMPORTED)
-find_library(NTS_cpp_LIBRARY_PATH NTS_cpp HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
-set_target_properties(NTS_cpp PROPERTIES IMPORTED_LOCATION "${NTS_cpp_LIBRARY_PATH}")
